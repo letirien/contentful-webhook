babd1b5b03d7a52f2fb9ec41eb9bf7f711b80f56
titien <titien.schott.67@gmail.com>
2023-05-26 17:23:18 +0200

    test with index

--- src/api.js
+++ src/api.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const serverless = require("serverless-http");
-
 const app = express();
 const router = express.Router();
+
+const algoliasearch = require('algoliasearch');
+const Contentful = require('contentful') 
 
 router.get("/", (req, res) => {
   res.json({
     hello: "hi!"
   });
 });
+
+// post for projects
 router.post('/contentful-webhook/index/:data', (req, res)=>{
-  const projet = req.body;
+  // const projet = req.body;
+  // Extrait les informations nécessaires du projet
+  const objectID = req.body.id;
+  const title  = req.body.title;
+  const subtitle  = req.body.subtitle;
+  const hook  = req.body.hook;
+
+  const object = { objectID, title, subtitle, hook/* autres champs */ };
   if(req.body){
-    res.sendStatus(200);
+    index.saveObject(object);
   }else{
     res.sendStatus(500);
   }
